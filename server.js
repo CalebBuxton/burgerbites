65837c7642a5ac449125ddc3163c6fe25f490c7c
unknown <tacmb@ELMV0153.int.mcmaster.com>
2017-08-10 10:24:26 -0400

    added all connections and routes to server

--- server.js
+++ server.js
@@ -1,11 +1,18 @@
 var express = require("express");
 var bodyParser = require("body-parser");
-var method = require("method-override")
+var methodOverride = require("method-override")
+var exphbs = require("express-handlebars");
+var routes = require("./app/controllers/bugers_controller.js")
 
 var app = express();
 var PORT = process.env.PORT || 8080;
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.text());
-app.use(bodyParser.json({ type: "application/vnd.api+json" }));
+app.use(express.static("public"));
+app.use(bodyParser.urlenvoded({ extended: false }));
+app.use(methodOverride("_method"));
+
+app.engine("handlebars", exphbs({ defaultLayout: "main"} ));
+app.set("view engine", "handlebars");
+
+app.use("/", routes);
+app.listen(port);
